86ed49f87fb8e5d84a00cd0a5e1f9878aad4a6a2
Parvesh Mishra <parvesh225@outlook.com>
2023-09-02 16:15:42 +0530

    uncommitted files

--- server/utils/db.utill.js
+++ server/utils/db.utill.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = require('../config/key');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
+const sequelize = new Sequelize('director-dashboard', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql'
   });
 
