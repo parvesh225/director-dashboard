ebad980ef276307a98b20e8847c46fc462a35d47
Parvesh Mishra <parvesh225@gmail.com>
2023-09-04 10:24:35 +0530

    Activity graph, center and project wise button implementation

--- client/src/components/pages/WelcomePage.js
+++ client/src/components/pages/WelcomePage.js
@@ -4,6 +4,15 @@ import Header from "../layout/Header";
 import Sidebar from "../layout/Sidebar";
 import './WelcomePage.css';
 import Chart from "react-apexcharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 const axios = require("axios").default;
 
 
@@ -12,6 +21,10 @@ class WelcomePage extends Component {
     super(props);
 
     this.state = {
+      centre: [],
+      project: [],
+      centre_name: "",
+      project_name: "",
       totalFunding: 0,
       totalProject: 0,
       totalEmployee: 0,
@@ -19,58 +32,98 @@ class WelcomePage extends Component {
       totalMou: 0,
 
       //series: [44, 55, 13, 43, 22],
-      series:[],
+      series: [],
       options: {},
-    
-      series1: [{
-        name: 'Net Profit',
-        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-      }, {
-        name: 'Revenue',
-        data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-      }, {
-        name: 'Free Cash Flow',
-        data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-      }],
-      options1: {
+
+      series1: [],
+      options1: {},
+
+    // for second row graph
+      series2Row: [],
+      options2Row: {
         chart: {
-          type: 'bar',
-          height: 350
+          height: 350,
+          type: 'radialBar',
         },
         plotOptions: {
-          bar: {
-            horizontal: true,
-            columnWidth: '55%',
-            endingShape: 'rounded'
+          radialBar: {
+            hollow: {
+              size: '70%',
+            }
           },
         },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          show: true,
-          width: 2,
-          colors: ['transparent']
-        },
-        xaxis: {
-          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-        },
-        yaxis: {
-          title: {
-            text: '$ (thousands)'
-          }
-        },
-        fill: {
-          opacity: 1
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return "$ " + val + " thousands"
-            }
-          }
-        }
+        labels: ['MOU'],
+        colors: ['#FFA556'],
       },
+
+      series22Row: [],
+      options22Row: {
+        chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              size: '70%',
+            }
+          },
+        },
+        labels: ['Geographic'],
+        colors: ['#AC86D1'],
+      },
+
+      series23Row: [],
+      options23Row: {
+        chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              size: '70%',
+            }
+          },
+        },
+        labels: ['Advocacy'],
+        colors: ['#9BEA81'],
+
+      },
+
+      series24Row: [],
+      options24Row: {
+        chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              size: '70%',
+            }
+          },
+        },
+        labels: ['Knowledge'],
+        colors: ['#208293'] 
+      },
+
+      series25Row: [],
+      options25Row: {
+        chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              size: '70%',
+            }
+          },
+        },
+        labels: ['Other'],
+      },
+
     };
   }
 
@@ -78,25 +131,34 @@ class WelcomePage extends Component {
   componentDidMount() {
     var thizz = this;
 
-    axios.get(process.env.REACT_APP_BASE_URL + "/api/landing-page" )
+    axios.get(process.env.REACT_APP_BASE_URL + "/api/landing-page")
       .then((response) => {
         console.log(response.data);
+
+        var centre = response.data.centres
+        var project = response.data.projects
+
+        this.setState({
+          centre: centre,
+          project: project
+        })
+
         var actualResp = response.data;
         if (actualResp.totalFund) {
           thizz.setState({ totalFunding: actualResp.totalFund })
-      }
+        }
         if (actualResp.totalProject) {
           thizz.setState({ totalProject: actualResp.totalProject })
-      }
+        }
         if (actualResp.totalEmployee) {
           thizz.setState({ totalEmployee: actualResp.totalEmployee })
-      }
+        }
         if (actualResp.totalKnowledgeProducts) {
           thizz.setState({ knowledgeProduct: actualResp.totalKnowledgeProducts })
-      }
+        }
         if (actualResp.totalMou) {
           thizz.setState({ totalMou: actualResp.totalMou })
-      }
+        }
         if (actualResp.FundingAgency) {
           let newOptions = {
             chart: {
@@ -108,7 +170,7 @@ class WelcomePage extends Component {
               breakpoint: 480,
               options: {
                 chart: {
-                  width: 200
+                  width: 200,
                 },
                 legend: {
                   position: 'bottom'
@@ -117,19 +179,141 @@ class WelcomePage extends Component {
             }]
           }
           thizz.setState({ options: newOptions })
-      }
+        }
         if (actualResp.FundingAmt) {
           thizz.setState({ series: actualResp.FundingAmt })
-      }
+        }
 
-      
+        if (actualResp.sanctionFund && actualResp.releasedFund && actualResp.utilizationFund) {
+          let newSeries = [{
+            name: 'Sanction Fund',
+            data: actualResp.sanctionFund
+          }, {
+            name: 'Released Fund',
+            data: actualResp.releasedFund
+          }, {
+            name: 'Utilization Fund',
+            data: actualResp.utilizationFund
+          }];
+          thizz.setState({ series1: newSeries })
+        }
+
+        if (actualResp.projectList) {
+          let secondOpt = {
+            chart: {
+              type: 'bar',
+              height: 350
+            },
+            plotOptions: {
+              bar: {
+                horizontal: true,
+                columnWidth: '55%',
+                endingShape: 'rounded'
+              },
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              show: true,
+              width: 2,
+              colors: ['transparent']
+            },
+            xaxis: {
+              categories: actualResp.projectList,
+            },
+            yaxis: {
+              title: {
+                text: "In crores"
+              }
+            },
+            fill: {
+              opacity: 1
+            },
+            tooltip: {
+              y: {
+                formatter: function (val) {
+                  return "&#x20B9; " + val + " crores"
+                }
+              }
+            }
+          }
+          thizz.setState({ options1: secondOpt })
+        }
+
+        var series2Row = thizz.state.series2Row;
+        series2Row = response.data.projectActivity[0].series;
+
+        var catagoryName2Row = thizz.state.catagoryName2Row;
+        catagoryName2Row = response.data.projectActivity[0].name;
+        var activity2Row = thizz.state.activity2Row;
+        activity2Row = response.data.projectActivity[0].label
+
+        var series22Row = thizz.state.series22Row;
+        series22Row = response.data.projectActivity2[0].series;
+        var catagoryName22Row = thizz.state.catagoryName22Row;
+        catagoryName22Row = response.data.projectActivity2[0].name;
+        var activity22Row = thizz.state.activity22Row;
+        activity22Row = response.data.projectActivity2[0].label
+
+        var series23Row = thizz.state.series23Row;
+        series23Row = response.data.projectActivity3[0].series;
+        var catagoryName23Row = thizz.state.catagoryName23Row;
+        catagoryName23Row = response.data.projectActivity3[0].name;
+        var activity23Row = thizz.state.activity23Row;
+        activity23Row = response.data.projectActivity3[0].label
+
+        var series24Row = thizz.state.series24Row;
+        series24Row = response.data.projectActivity4[0].series;
+        var catagoryName24Row = thizz.state.catagoryName24Row;
+        catagoryName24Row = response.data.projectActivity4[0].name;
+        var activity24Row = thizz.state.activity24Row;
+        activity24Row = response.data.projectActivity4[0].label
+
+        var series25Row = thizz.state.series25Row;
+        series25Row = response.data.projectActivity5[0].series;
+        var catagoryName25Row = thizz.state.catagoryName25Row;
+        catagoryName25Row = response.data.projectActivity5[0].name;
+        var activity25Row = thizz.state.activity25Row;
+        activity25Row = response.data.projectActivity5[0].label
+
+        thizz.setState({
+          series2Row: series2Row,
+          catagoryName2Row: catagoryName2Row,
+          activity2Row: activity2Row,
+          series22Row: series22Row,
+          catagoryName22Row: catagoryName22Row,
+          activity22Row: activity22Row,
+          series23Row: series23Row,
+          catagoryName23Row: catagoryName23Row,
+          activity23Row: activity23Row,
+          series24Row: series24Row,
+          catagoryName24Row: catagoryName24Row,
+          activity24Row: activity24Row,
+          series25Row: series25Row,
+          catagoryName25Row: catagoryName25Row,
+          activity25Row: activity25Row,
+
+        });
+
+
       }).catch((error) => {
         console.log(error.response.data)
       })
-      
+
   }
 
   render() {
+
+    let centre = this.state.centre.map(function (center, index) {
+
+      return <option key={center.id} value={center.centre_code}>{center.centre_name}</option>;
+    })
+    let project = this.state.project.map(function (project, index) {
+      return <option key={project.id} value={project.project_code}>{project.project_name}</option>;
+
+    })
+
     return (
       <>
         <Header />
@@ -137,7 +321,32 @@ class WelcomePage extends Component {
         <div className="content-wrapper">
           <section className="content">
             <div className="heading">
-              <p className="headingLine"> DIRECTOR NIUA DASHBOARD</p>
+              <span className="headingLine"> DIRECTOR NIUA DASHBOARD  </span>
+              <span className="filterButton">
+                <div className="myContainer">
+                  <select
+                    className="form-control select-box"
+                    name="centre_name"
+                    id="centre_name"
+                    value={this.state.centre_name}
+                  // onChange={this.handleChange}
+                  >
+                    <option> All Centers </option>
+                    {centre}
+                  </select>
+
+                  <select
+                    className="form-control select-box"
+                    name="project_name"
+                    id="project_name"
+                    value={this.state.project_name}
+                  // onChange={this.handleChange}
+                  >
+                    <option selected="selected"> All Projects </option>
+                    {project}
+                  </select>
+                </div>
+              </span>
             </div>
             {/* <div className="container-fluid"> */}
             <div className="header-container card">
@@ -148,50 +357,112 @@ class WelcomePage extends Component {
               <div className="row text-center mt-4">
                 <div className="col-md-2">
                   <p className="p1">Total Fund</p>
-                  <p className="p2"> { this.state.totalFunding } </p>
+                  <p className="p2"> {this.state.totalFunding} </p>
                 </div>
                 <div className="col-md-2">
                   <p className="p1">Total Project</p>
-                  <p className="p2"> { this.state.totalProject } </p>
+                  <p className="p2"> {this.state.totalProject} </p>
                 </div>
                 <div className="col-md-2">
                   <p className="p1">Total Employee</p>
-                  <p className="p2"> { this.state.totalEmployee } </p>
+                  <p className="p2"> {this.state.totalEmployee} </p>
                 </div>
                 <div className="col-md-2">
                   <p className="p1">Knowledge Products</p>
-                  <p className="p2"> { this.state.knowledgeProduct } </p>
+                  <p className="p2"> {this.state.knowledgeProduct} </p>
                 </div>
                 <div className="col-md-2">
                   <p className="p1">Total MOU</p>
-                  <p className="p2"> { this.state.totalMou } </p>
+                  <p className="p2"> {this.state.totalMou} </p>
                 </div>
               </div>
             </div>
 
             <div className="row">
               <div className="col-md-6">
-              <div className="header-container card">
-              <div className="icon">
-                <i className="fa fa-bar-chart faChart" aria-hidden="true"></i>
-              </div>
-              <h4 className="header"> Overall Fund</h4>
-                <Chart options={this.state.options} series={this.state.series} type="pie" width={500} />
-            </div>
+                <div className="header-container card">
+                  <div className="icon">
+                    <i className="fa fa-bar-chart faChart" aria-hidden="true"></i>
+                  </div>
+                  <h4 className="header"> Overall Fund</h4> <span style={{float:"right", marginTop:"10px"}}> <input type="checkbox" /> <lable> Project Wise Funding</lable> </span>
+                  <Chart options={this.state.options} series={this.state.series} type="pie" style={{ height: "315px" }} />
+                </div>
               </div>
               <div className="col-md-6">
-              <div className="header-container card">
-              <div className="icon">
-                <i className="fa fa-bar-chart faChart" aria-hidden="true"></i>
-              </div>
-              <h4 className="header">Fund Utilization</h4>
-                <Chart options={this.state.options1} series={this.state.series1} type="bar" height={355} />
-            </div>
+                <div className="header-container card">
+                  <div className="icon">
+                    <i className="fa fa-bar-chart faChart" aria-hidden="true"></i>
+                  </div>
+                  <h4 className="header">Fund Utilization</h4>
+                  <Chart options={this.state.options1} series={this.state.series1} type="bar" height={300} />
+                </div>
               </div>
             </div>
 
+
+            <div className="row text-center p-2">
+                      <div className="col-md-2 card bg-primary">
+
+                        <p style={{ marginTop: "70px" }}> Progress </p>
+                        <p style={{ marginTop: "100px" }}> Activity </p>
+                      </div>
+
+                      <div className="col-md-2 card">
+                        <Chart
+                         options={this.state.options2Row} series={this.state.series2Row}
+                          type="radialBar"
+                          height={200}
+                        />
+                        <p className="mb-5">  {this.state.catagoryName2Row} </p> 
+                        <br/>
+                        <p className="p-1" style={{backgroundColor:"#FFA556", color:"white"}}> {this.state.activity2Row}</p>
+                      </div>
+
+                      <div className="col-md-2 card">
+                        <Chart
+                         options={this.state.options22Row} series={this.state.series22Row}
+                          type="radialBar"
+                          height={200}
+                        />
+                        <p className="mb-4">  {this.state.catagoryName22Row} </p> 
+                        <br/>
+                        <p className="p-1" style={{backgroundColor:"#AC86D1", color:"white"}}> {this.state.activity22Row}</p>
+                      </div>
+                      <div className="col-md-2 card">
+                        <Chart
+                         options={this.state.options23Row} series={this.state.series23Row}
+                          type="radialBar"
+                          height={200}
+                        />
+                        <p className="mb-4"> {this.state.catagoryName23Row} </p>
+                        <br/>
+                        <p className="p-1" style={{backgroundColor:"#9BEA81", color:"white"}}> {this.state.activity23Row}</p>
+                      </div>
+                      <div className="col-md-2 card">
+                        <Chart
+                         options={this.state.options24Row} series={this.state.series24Row}
+                          type="radialBar"
+                          height={200}
+                        />
+                        <p className="mb-4">  {this.state.catagoryName24Row}  </p>
+                        <br/>
+                        <p className="p-1" style={{backgroundColor:"#208293", color:"white"}}> {this.state.activity24Row}</p>
+                      </div>
+                      <div className="col-md-2 card">
+                        <Chart
+                         options={this.state.options25Row} series={this.state.series25Row}
+                          type="radialBar"
+                          height={200}
+                        />
+                        <p className="mb-4">   {this.state.catagoryName25Row}  </p>
+                        <p className="p-1 bg-primary"> {this.state.activity25Row}</p>
+                      </div>
+                </div>
+
+                
+
             {/* </div> */}
-          </section>
+          </section>  
         </div>
         <Footer />
       </>
--- server/controllers/api/welcome.controller.js
+++ server/controllers/api/welcome.controller.js
@@ -1,14 +1,39 @@
 const { ProjectActivityTask } = require("../../models/ProjectActivityTask");
 const { ProjectPlan } = require("../../models/ProjectPlan");
 const { Team } = require("../../models/Team");
+const { Centre } = require("../../models/Centre");
+const { Project } = require("../../models/Project");
+const { ProjectActivity } = require("../../models/ProjectActivity");
 const { db } = require("../../utils/db.utill");
 
+
+function formatNumberToCrores(number) {
+  if (isNaN(number)) {
+    return "Invalid Number";
+  }
+
+  const croreValue = 10000000; // 1 Crore
+  const formattedNumber = (number / croreValue).toFixed(2); // Round to 2 decimal places
+  return formattedNumber;
+}
 
 async function overView(req, res, next) {
 
   try {
-    let FundingAgency = [];
-    let FundingAmt = [];
+    let fundingAgency = [];
+    let fundingAmt = [];
+    let sanctionFund = [];
+    let releasedFund = [];
+    let utilizationFund = [];
+    let projectList = [];
+    let customObj = [];
+    let customObj2 = [];
+    let customObj3 = [];
+    let customObj4 = [];
+    let customObj5 = [];
+
+    const centres = await Centre.findAll();
+    const projects = await Project.findAll();
 
     let totalAllocatedBudget = await ProjectPlan.sum('allocated_budget');
     let totalProject = await ProjectPlan.count({
@@ -37,27 +62,136 @@ async function overView(req, res, next) {
         project_activity_id: 28,
       }
     });
-      // Get all funding details for pie graph
+    // Get all funding details for pie graph
     const FundingAgancyDetails = await db.query(`SELECT * FROM project_plan as pp left JOIN funding_agency as fa ON pp.funding_agency = fa.agency_code`,
-    { type: db.QueryTypes.SELECT });
+      { type: db.QueryTypes.SELECT });
 
-    
-    //bind data according to graph
+    //bind data according to Overall Fund graph
     for (let i = 0; i < FundingAgancyDetails.length; i++) {
       let agency = FundingAgancyDetails[i];
-      FundingAgency.push(agency.agency_name);
-      FundingAmt.push(agency.allocated_budget);
-    } 
+      fundingAgency.push(agency.agency_name);
+      fundingAmt.push(agency.allocated_budget);
+    }
+
+    //get data sanction fund and released fund
+    //second graph for welcome page
+    const sanction_releasedFund = await db.query(`SELECT t1.centre_name, t1.project_name as project_code,t3.project_name as project_name, t1.allocated_budget ,SUM(t2.amount_recieved) AS released_amt FROM project_plan t1 INNER JOIN finance_recieved t2 ON t1.id = t2.project_plan_id INNER JOIN project t3 ON t1.project_name = t3.project_code WHERE t2.is_adjustment IS NULL OR t2.is_adjustment = 0`,
+      { type: db.QueryTypes.SELECT });
+
+    if (sanction_releasedFund.length > 0) {
+      for (let j = 0; j < sanction_releasedFund.length; j++) {
+        let fund = sanction_releasedFund[j];
+        // number formetted in crores
+         let s = formatNumberToCrores(fund.allocated_budget);
+          let r = formatNumberToCrores(fund.released_amt);
+
+        sanctionFund.push(s);
+        releasedFund.push(r);
+        projectList.push(fund.project_name);
+      }
+    }
+
+    let utilizeFund = await db.query(`SELECT table1.centre_name, table1.project_name, SUM(table3.expenditure)As fund_utilized
+    FROM project_plan table1
+    INNER JOIN finances table3 ON table1.id = table3.project_plan_id
+    GROUP BY table1.id`,
+      { type: db.QueryTypes.SELECT });
+
+    for (let k = 0; k < utilizeFund.length; k++) {
+      let u = formatNumberToCrores(utilizeFund[k].fund_utilized);
+      utilizationFund.push(u);
+    }
+
+    //for 2nd row graph 
+    let subCatagoryList = await ProjectActivity.findAll();
+
+    for (i = 0; i < subCatagoryList.length; i++) {
+      var obj = subCatagoryList[i];
+
+      // for Mou
+      if (obj.project_master_activity_id === 1) {
+        //get round figure value for series
+        let x = (obj.current_entries * 100) / obj.expected_entries
+        customObj.push({
+          name: obj.project_master_activity_name,
+          totalActivities: obj.expected_entries,
+          currentActivities: obj.current_entries,
+          series: [Math.trunc(x)],
+          label: "Activity (" + obj.current_entries + "/" + obj.expected_entries + ")"
+        })
+      }
+
+       // for Geographic Spread or Locations
+       if (obj.project_master_activity_id === 2) {
+        let x = (obj.current_entries * 100) / obj.expected_entries
+        customObj2.push({
+          name: obj.project_master_activity_name,
+          totalActivities: obj.expected_entries,
+          currentActivities: obj.current_entries,
+          series: [Math.trunc(x)],
+          label: "Activity (" + obj.current_entries + "/" + obj.expected_entries + ")"
+        })
+      }
+
+      // for Advocacy and Capacity Building
+      if (obj.project_master_activity_id === 3) {
+        let x = (obj.current_entries * 100) / obj.expected_entries;
+        customObj3.push({
+          name: obj.project_master_activity_name,
+          totalActivities: obj.expected_entries,
+          currentActivities: obj.current_entries,
+          series: [Math.trunc(x)],
+          label: "Activity (" + obj.current_entries + "/" + obj.expected_entries + ")"
+        })
+      }
+
+      // for Knowledge management
+      if (obj.project_master_activity_id === 4) {
+        let x = (obj.current_entries * 100) / obj.expected_entries;
+        customObj4.push({
+          name: obj.project_master_activity_name,
+          totalActivities: obj.expected_entries,
+          currentActivities: obj.current_entries,
+          series: [Math.trunc(x)],
+          label: "Activity (" + obj.current_entries + "/" + obj.expected_entries + ")"
+        })
+      }
+
+      // for Other (such as Prog/Proj devt: partnerships)
+      if (obj.project_master_activity_id === 5) {
+        let x = (obj.current_entries * 100) / obj.expected_entries;
+        customObj5.push({
+          name: obj.project_master_activity_name,
+          totalActivities: obj.expected_entries,
+          currentActivities: obj.current_entries,
+          series: [Math.trunc(x)],
+          label: "Activity (" + obj.current_entries + "/" + obj.expected_entries + ")"
+        })
+      }
+    }
 
     return res.status(200).json({
       status: true,
-      totalFund: totalAllocatedBudget,
-      totalProject: totalProject,
-      totalEmployee: totalEmployee,
-      totalKnowledgeProducts: totalKnowledgeProducts,
-      totalMou: totalMou,
-      FundingAgency:FundingAgency,
-      FundingAmt:FundingAmt
+      "centres": centres,
+      "projects": projects,
+      "totalFund": totalAllocatedBudget,
+      "totalProject": totalProject,
+      "totalEmployee": totalEmployee,
+      "totalKnowledgeProducts": totalKnowledgeProducts,
+      "totalMou": totalMou,
+      "FundingAgency": fundingAgency,
+      "FundingAmt": fundingAmt,
+      //second graph for welcome page
+      projectList: projectList,
+      "sanctionFund": sanctionFund,
+      "releasedFund": releasedFund,
+      "utilizationFund": utilizationFund,
+      //2nd row graph
+      "projectActivity":customObj,
+      "projectActivity2": customObj2,
+      "projectActivity3": customObj3,
+      "projectActivity4": customObj4,
+      "projectActivity5": customObj5,
     })
 
   } catch (err) {
